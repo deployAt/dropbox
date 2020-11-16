dd23675aa2d89c53bd0d4e17cd164e7f1e5e4c1f
deployAt <mikowhy@tutanota.com>
2020-11-16 21:50:14 +0100

    feat: add DStorage contract

--- contracts/DStorage.sol
+++ contracts/DStorage.sol
@@ -3,11 +3,58 @@ pragma solidity ^0.6.0;
 
 contract DStorage {
     string public name = "DStorage";
+    uint256 public fileCount = 0;
     mapping(uint256 => File) public files;
 
     struct File {
+        uint256 fileId;
+        string fileHash;
+        uint256 fileSize;
+        string fileType;
         string fileName;
+        string fileDescription;
+        uint256 uploadTime;
+        address payable uploader;
     }
+    event FileUploaded(
+        uint256 fileId,
+        string fileHash,
+        uint256 fileSize,
+        string fileType,
+        string fileName,
+        string fileDescription,
+        uint256 uploadTime,
+        address payable uploader
+    );
 
     constructor() public {}
+
+    function uploadFile(
+        string memory _fileHash,
+        uint256 _fileSize,
+        string memory _fileType,
+        string memory _fileName,
+        string memory _fileDescription
+    ) public {
+        require(bytes(_fileHash).length > 0);
+        require(_fileSize > 0);
+        require(bytes(_fileType).length > 0);
+        require(bytes(_fileName).length > 0);
+        require(bytes(_fileDescription).length > 0);
+        require(msg.sender != address(0));
+
+        fileCount = fileCount + 1;
+        files[fileCount] = File(
+            fileCount,
+            _fileHash,
+            _fileSize,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            now,
+            msg.sender
+        );
+
+        emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+    }
 }
